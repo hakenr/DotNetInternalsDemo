3225d052c5b280480e641d7b7d82bf70d9fb26bb
Robert Haken <haken@havit.cz>
2021-04-27 12:46:22 +0200

    dotnet-stack

---
+++ DotnetStackToolDemo/DotnetStackToolDemo_Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+long x = 0;
+
+Task.Run(() =>
+{
+	while (true)
+	{
+		DoSomething();
+	}
+});
+
+void DoSomething()
+{
+	x++;
+	Task.Delay(50);
+}
+
+Console.ReadLine();
+
+
+// dotnet tool install -g dotnet-stack
+// dotnet-stack ps
+// dotnet-stack report -p <id>
+// https://github.com/dotnet/diagnostics/blob/main/documentation/design-docs/dotnet-tools.md#dotnet-stack
