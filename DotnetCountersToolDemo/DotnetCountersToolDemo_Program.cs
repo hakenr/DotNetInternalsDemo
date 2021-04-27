b49de250159a1d64b3d26f5ae26827f1e9c3f799
Robert Haken <haken@havit.cz>
2021-04-27 13:12:23 +0200

    dotnet-counters

---
+++ DotnetCountersToolDemo/DotnetCountersToolDemo_Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+long x = 0;
+
+Task.Run(() =>
+{
+	while (true)
+	{
+		DoSomething();
+	}
+});
+
+void DoSomething()
+{
+	x++;
+	var z = new byte[512];
+	Task.Delay(50);
+}
+
+Console.ReadLine();
+
+
+// dotnet tool install -g dotnet-counters
+// dotnet-counters ps
+// dotnet-counters list
+// dotnet-counters monitor -p <id> [counters]
+// dotnet - counters monitor - n iisexpress
+// https://github.com/dotnet/diagnostics/blob/main/documentation/design-docs/dotnet-tools.md#dotnet-counters
