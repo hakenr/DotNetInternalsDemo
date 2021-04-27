b6852e4762aff19e2ec8b377986f4bc25c073b76
Robert Haken <haken@havit.cz>
2021-04-27 23:27:27 +0200

    Pinned Object Heap (POH)

---
+++ PinnedHeapDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+//var pinnedArray = GC.AllocateArray<byte>(length: 1024 * 1024, pinned: true);
+var pinnedArray = GC.AllocateUninitializedArray<byte>(length: 1024 * 1024, pinned: true);
+
+Encoding.ASCII.GetBytes("ABCDEFGHIJKLMNOPQRSTUVWXYZ").CopyTo(pinnedArray, 0);
+
+GC.Collect();
+Debugger.Break();
+
+pinnedArray = null; // run in RELEASE!
+
+GC.Collect();
+Debugger.Break();
+
+// dumpheap -stat
+// do
+// gcroot
+// eeheap -gc
