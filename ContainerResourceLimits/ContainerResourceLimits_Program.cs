ca0f800067650e53cc2cee42be1fcb90183bf0e2
Robert Haken <haken@havit.cz>
2021-04-28 01:29:41 +0200

    ContainerResourceLimits

---
+++ ContainerResourceLimits/ContainerResourceLimits_Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+
+Debugger.Break();
+
+var large = new byte[50 * 1024 * 1024];  // 50MB
+
+Debugger.Break();
+
+
+// https://docs.microsoft.com/en-us/dotnet/core/run-time-config/garbage-collector
+// runtimeconfig.template.json
+
