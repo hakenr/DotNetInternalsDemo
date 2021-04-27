022a0cde1dcb523277fd23e59dc216f8908afcb4
Robert Haken <haken@havit.cz>
2021-04-27 18:28:36 +0200

    GC.GetGCMemoryInfo()

---
+++ GCGetGCMemoryInfoDemo/GCGetGCMemoryInfoDemo_Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+
+const int count = 1000;
+
+MyClass instance;
+for (int i = 0; i < count; i++)
+{
+	instance = new MyClass();
+}
+
+ShowMemoryInfo();
+Console.ReadLine();
+GC.Collect();
+ShowMemoryInfo();
+
+void ShowMemoryInfo()
+{
+	var info = GC.GetGCMemoryInfo(); // <======
+
+	Console.WriteLine(JsonConvert.SerializeObject(info, Formatting.Indented));
+	var gen = 0;
+	foreach (var generationInfo in info.GenerationInfo)
+	{
+		Console.WriteLine($"Generation {gen}:");
+		Console.WriteLine(JsonConvert.SerializeObject(generationInfo, Formatting.Indented));
+		gen++;
+	}
+}
+public class MyClass
+{
+	private byte[] arr = new byte[1024];
+}
