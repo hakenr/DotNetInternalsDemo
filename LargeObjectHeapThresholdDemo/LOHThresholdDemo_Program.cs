9b3497c39a4222c334a99019107a4f694c80596b
Robert Haken <haken@havit.cz>
2022-04-26 12:42:14 +0200

    LargeObjectHeapThresholdDemo cleanup

--- LargeObjectHeapThresholdDemo/LOHThresholdDemo_Program.cs
+++ LargeObjectHeapThresholdDemo/LOHThresholdDemo_Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 
 byte[] empty = new byte[0]; // 24 bytes
 byte[] soh = new byte[85000 - 25];
@@ -9,13 +8,6 @@ byte[] test = new byte[0x1D4C0];
 Console.WriteLine($"soh Generation: " + GC.GetGeneration(soh));
 Console.WriteLine($"loh Generation: " + GC.GetGeneration(loh));
 Console.WriteLine($"test Generation: " + GC.GetGeneration(test));
-Debugger.Break();
 
-soh = null;
-loh = null;
-GC.Collect(); // rooted by CPU regs
-Debugger.Break();
-
-
-// COMPlus_GCLOHThreshold environment variable (HEX! 1D4C0 = 120_000)
+// COMPlus_GCLOHThreshold environment variable (HEX! 1D4C0 = 120_000) via launchSettings.json
 // https://docs.microsoft.com/en-us/dotnet/core/run-time-config/garbage-collector
