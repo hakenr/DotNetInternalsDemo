b0bcf0294065d92548c4ca43ba7f48d9b4841c46
Robert Haken <haken@havit.cz>
2022-04-26 11:11:30 +0200

    dotnet-gcdump, dotnet-trace

---
+++ DotnetGcDumpToolDemo/DotnetGcDumpToolDemo_Program.cs
@@ -0,0 +1,33 @@
+﻿int a = 0xAAAA;
+bool b = true;
+Int64 c = 0xCCCCCCCC;
+for (int i = 0; i < 0xFF; i++)
+{
+	MyClass instance = new MyClass(a);
+}
+int r;
+
+Console.WriteLine("Collect gcdump and press ENTER to end...");
+Console.ReadLine();
+
+public class MyClass
+{
+	private static MyClass lastInstance;
+	private int field1;
+	private int field2;
+	private int field3;
+	private byte[] data = new byte[2048];
+
+	public MyClass(int field)
+	{
+		field1 = field;
+		lastInstance = this;
+	}
+}
+
+
+// dotnet tool install -g dotnet-gcdump
+// dotnet-gcdump ps
+// dotnet-gcdump collect -p <id>
+// dotnet-gcdump report <path>
+// .gcdump file -> VisualStudio / PerfView
---
+++ DotnetTraceToolDemo/DotnetTraceToolDemo_Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+long x = 0;
+
+Task.Run(() =>
+{
+	while (true)
+	{
+		DoSomething();
+	}
+});
+
+void DoSomething()
+{
+	x++;
+	Task.Delay(50);
+}
+
+Console.ReadLine();
+
+
+// dotnet tool install -g dotnet-trace
+// dotnet-trace ps
+// dotnet-trace collect -p <id>
+// dotnet-trace report <file> topN
+// https://github.com/dotnet/diagnostics/blob/main/documentation/design-docs/dotnet-tools.md#dotnet-trace
